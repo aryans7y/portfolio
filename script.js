6caffd519949fb15ff184f6c5d6c8a7ca4f46875
Aryan Yadav <aryanyadav9085@gmail.com>
2025-08-15 00:16:47 -0800

    Add files via upload
    This is my portfolio

---
+++ script.js
@@ -0,0 +1,54 @@
+//                   toggle menu icon
+let menuIcon= document.querySelector('#menu-icon');
+let navbar= document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('active');
+    navbar.classList.toggle('active');
+};
+    
+// scroll section active link
+// Select all sections and navigation links
+let section= document.querySelectorAll('section');
+let navlink= document.querySelectorAll('header nav a');
+window.onscroll=()=>{
+    section.forEach(sec=>{
+        let top= window.scrollY;
+        let offset= sec.offsetTop - 150;
+        let height= sec.offsetHeight;
+        let id= sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navlink.forEach(links=>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*='+id+']').classList.add('active');
+            });
+        };
+    });
+    // sticky navbar
+    let header= document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+    // remove toggle icon and navbar when click navbar link (scroll)
+    menuIcon.classList.remove('active');
+    navbar.classList.remove('active');
+};
+// scroll reveal
+ScrollReveal({
+//    reset: true,
+   distance: '80px',
+   duration: 2000,
+   delay: 200
+});
+ScrollReveal().reveal('.content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form' , { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img' , { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-contect', { origin: 'right' });
+
+const typed = new Typed('.mutliple-text', {
+    strings: ['Web Developer', 'Full Stack Developer', 'Software Engineer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
